cb567800cdc51eead5c5685064377949f4f81e10
taoyu22633 <taoyu22633@hundsun.com>
2023-04-26 10:03:38 +0800

    修改

---
+++ xd-service-account-app/src/main/java/com/xindong/accounting/StarterAppApplication.java
@@ -0,0 +1,13 @@
+package com.xindong.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.xindong")
+public class StarterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterAppApplication.class, args);
+	}
+
+}
